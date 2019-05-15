3f3b08943885e6aa21eb594f98dc48256127015a
Paul Dellinger <pd88@login-teer-15.oit.duke.edu>
2019-05-14 21:28:42 -0400

    some comments, some cleaning up ;

--- nytpuzzle.c
+++ nytpuzzle.c
@@ -8,60 +8,90 @@ typedef struct word{
   char letters[100];
   struct word *next;
 } word_t;
+//word struct is a linked list of strings
+
+
 
 bool test1(char* letters, char* word);
 bool test2(char center_letter, char* word);
 bool check(char* wordie, char start, char*left, char * right, char * top, char * bottom);
-char get_side(char letter, char * left, char * right, char *top, char * bottom);
-void recurse(int letters_used, char start, word_t* wordlist, word_t * all_words, char*left, char * right, char * top, char * bottom, char* letters);
+int get_side(char letter, char * left, char * right, char *top, char * bottom);
 void base(char *letters, char *word, word_t* wordlist);
-word_t * addword(char * word, word_t* wordlist);
+/*
+Solver for nytimes.com/puzzles/spelling-bee and nytimes.com/puzzles/letter-boxed.
 
+Spelling Bee Rules:
+-Create words using the given letters.
+-Words must contain at least 4 letters.
+-Words must include the center letter.
+-Words may not be proper nouns or hyphenated.
+-Letters can be used more than once.
+
+Letter Boxed Rules:
+-Connect letters to spell words
+-Words must be at least 3 letters long
+-Letters can be reused
+-Consecutive letters cannot be from the same side
+-The last letter of a word becomes the first letter of the next word
+-e.g. THY > YES > SINCE
+-Use all letters to solve!
+
+Spelling bee is solved by going through every word in the dictionary and printing the
+ones that fit the criteria. Letterboxed is solved by identifying the one word
+solutions to the box, then all the two word solutions to the box. There has always
+be a two-word solution to letterboxed.
+
+*/
 
 int main(int argc, char** argv)
 {
-  char center_letter;
+
+  char center_letter[2];
   char write[3];
-  FILE *file = fopen(argv[1], "rt");
+
+  FILE *file = fopen(argv[1], "rt");  //input dictionary in comand line
   char type[3];
   char letters[8];
-  printf("Spelling Bee (sb) or Letterbox (lb)?\n>>");
+  printf("Spelling Bee (sb) or Letterbox (lb)?\n:");
   scanf("%s", &type);
-  printf("you have selected %s\n", type);
-  if (strcmp(type, "sb")==0){
+
+if (strcmp(type, "sb")==0){
+  printf("You have selected Spelling Bee!\n");
+
+  printf("Enter letters (not center): \n");
+  scanf("%s", &letters);
 
 
-    printf("enter letters: \n");
-    scanf("%s", &letters);
-
-    printf("the letters are: \n");
-    for(int i=0; i<7; i++){
-      printf("%c\n", letters[i]);
-      //printf("what's going on?");
-    }
-    printf("enter center letter: \n");
-    scanf(" %c", &center_letter);
-    printf("center letter is : %c\n", center_letter);
+  printf("Enter center letter: \n");
+  scanf(" %s", &center_letter);
+  printf("The letters are: \n");
+  for(int i=0; i<7; i++){
+    printf("%c\n", letters[i]);
+    //printf("what's going on?");
+  }
+  strcat(letters, center_letter);
+  printf("The center letter is : %s\n", center_letter);
 
 
   int count =0;
 
   if (file != NULL) {
-    char line[100];
-    char result[100];
-    while(1) {
-        count++;
-        fgets(line, sizeof(line), file);
-        if (getc(file)==EOF) break;
-        fseek(file, -1, SEEK_CUR);
-        //printf("word : %s", line);
-        if (test1(letters, line) && test2(center_letter,line)) printf("%s\n", line);
+      char line[100];
+      char result[100];
+      while(1) {
 
-      }
-    }
-
-      printf("words %d\n", count);
-    }
+          fgets(line, sizeof(line), file);
+          if (getc(file)==EOF) break;
+          fseek(file, -1, SEEK_CUR);
+          //printf("word : %s", line);
+          if (test1(letters, line) && test2(center_letter[0],line)){
+            printf("%s\n", line);
+            count++;
+          }
+        }
+      printf(" %d Words\n", count);
+}
+}
 if (strcmp(type, "lb")==0){
 
   char letters[13];
@@ -104,50 +134,31 @@ if (strcmp(type, "lb")==0){
     char line[100];
     char result[100];
     while(1) {
-        count++;
-        fgets(line, sizeof(line), file);
-        if (getc(file)==EOF) break;
+      count++;
+      fgets(line, sizeof(line), file);
+      if (getc(file)==EOF) break;
 
-        fseek(file, -1, SEEK_CUR);
-        word_t * word = (word_t*) malloc(sizeof(word_t));
-        strcpy(word->letters, line);
-        /*
-        for(int i=0; i<strlen(line)-1;i++){
-          word->letters[i]=line[i];
-        }
-        */
-        word->next = NULL;
-        if (all_words==NULL){
-          all_words = word;
-          tmp = word;
-        }
-        else{
-          tmp->next = word;
-          tmp = tmp->next;
-        }
+      fseek(file, -1, SEEK_CUR);
+      word_t * word = (word_t*) malloc(sizeof(word_t));
+      strcpy(word->letters, line);
 
-        //printf("word : %s", line);
-        //printf("%s\n", line);
-
+      word->next = NULL;
+      if (all_words==NULL){
+        all_words = word;
+        tmp = word;
+      }
+      else{
+        tmp->next = word;
+        tmp = tmp->next;
       }
     }
-    /*
-    while (all_words->next!=NULL){
-      printf("word: %s\n", all_words->letters);
-      all_words= all_words->next;
-    }
-    */
-      //printf("words %d\n", count);
+  }
 
-  /*
-  strcpy(wordie, "toner");
-  bool test = check(wordie, 't', left,right, top,bottom);
-  printf("test for doer: %d\n", check("doer", 't', left,right, top,bottom));
-  */
   word_t *wordlist = NULL;
   word_t * runner = all_words;
   word_t * runner2 = all_words;
   wordlist = (word_t*) malloc(sizeof(word_t));
+
   for (int i=0; i<13; i++){
   word_t * runner = all_words;
   while (runner!=NULL){
@@ -157,124 +168,122 @@ if (strcmp(type, "lb")==0){
     wordlist->next = NULL;
     if(check(runner->letters, letters[i], left, right, top, bottom)){
     //printf("starting with : %s\n", runner->letters);
-    runner2=all_words;
-    while(runner2!=NULL){
-      //printf("%s\n", runner2->letters);
-
-      if (check(runner2->letters, runner->letters[strlen(runner->letters)-3], left, right, top, bottom)){
-        //printf("comparing: %s and %s\n", runner->letters, runner2->letters);
-        base(letters, runner2->letters, wordlist);
-        word_t * runner3 = wordlist;
-        if(runner3->next != NULL){
+      base(letters, " ", wordlist);
+      word_t * runner3 = wordlist;
+      if(runner3->next != NULL){
         while(runner3->next->next!=NULL){
           runner3 = runner3->next;
         }
+        free(runner3->next);
         runner3->next = NULL;
-        }
-
       }
-      runner2=runner2->next;
+      runner2=all_words;
+      while(runner2!=NULL){
+        //printf("%s\n", runner2->letters);
+
+        if (check(runner2->letters, runner->letters[strlen(runner->letters)-3], left, right, top, bottom)){
+          //printf("comparing: %s and %s\n", runner->letters, runner2->letters);
+          base(letters, runner2->letters, wordlist);
+          word_t * runner3 = wordlist;
+          if(runner3->next != NULL){
+            while(runner3->next->next!=NULL){
+              runner3 = runner3->next;
+            }
+            free(runner3->next);
+            runner3->next = NULL;
+          }
+
+        }
+        runner2=runner2->next;
+      }
+    }
+    runner=runner->next;
     }
   }
-  runner=runner->next;
-}
-}
-  //recurse(0, 't', wordlist, all_words, left, right, top, bottom, letters);
-  /*
-  for (int i=0; i<13; i++){
-    \
-    wordlist->next = NULL;
-    //printf("calls recursion\n");
-    a = recurse(0, letters[i], wordlist, all_words, left, right, top, bottom, letters);
+  free(wordlist);
+  while(all_words!=NULL){
+    word_t* temp=NULL;
+    temp=all_words;
+    all_words = all_words->next;
+    free(temp);
+
   }
-  */
 
 }
-
 
 
 return 0;
 
-  }
+}
 
-  bool test1(char* letters, char* word){
-    //printf("testing %s", word);
-    //printf("apprent wordlength: %d",strlen(word));
-    if (strlen(word)<6) return false;
-    for (int i = 0; i < strlen(word)-2; i++){
-      bool hit = false;
-      char letter = word[i];
-      for (int k = 0; k < strlen(letters); k++){
-        //printf("letter :%c  compared to %c\n", letter, letters[k]);
-        if (letter == letters[k]){
-        //printf("hit!\n");
-        hit = true;
-          }
-      }
-      if(!hit){
-      //printf("miss!\n");
-      return false;
-    }
-  }
-    //printf("returning true\n");
-    return true;
-  }
-
-  bool test2(char center_letter, char* word){
+bool test1(char* letters, char* word){
+  if (strlen(word)<6) return false; //return false if under 4 letters
+  for (int i = 0; i < strlen(word)-2; i++){ //iterate letters of the word
     bool hit = false;
-    for (int i = 0; i < strlen(word); i++){
-
-      if (word[i]==center_letter) hit =true;
-
+    char letter = word[i];
+    for (int k = 0; k < strlen(letters); k++){//check if letters contain word letters
+      if (letter == letters[k]){
+        hit = true;
+      }
     }
-    //if (hit) printf("passed test 2");
-    return hit;
+    if(!hit)return false; //return false if letter not in letters
+  }
+  return true;
+}
+
+
+bool test2(char center_letter, char* word){
+  bool hit = false;
+  for (int i = 0; i < strlen(word); i++){
+    if (word[i]==center_letter) hit =true;
+  }
+  return hit;
+}
+
+bool check(char* wordie, char start, char*left, char * right, char * top, char * bottom){
+  if (wordie[0]!=start) return false;
+  if(get_side(start, left,right, top, bottom)<0) return false;
+  if (strlen(wordie)<5)return false;
+  for (int k=1; k<strlen(wordie)-2;k++){
+    if (get_side(wordie[k],left,right, top, bottom)==get_side(wordie[k-1],left,right, top, bottom) || get_side(wordie[k],left,right, top, bottom)<0) return false;
   }
 
-  bool check(char* wordie, char start, char*left, char * right, char * top, char * bottom){
-    if (wordie[0]!=start) return false;
-    if(get_side(start, left,right, top, bottom)<0) return false;
-    if (strlen(wordie)<5)return false;
-    for (int k=1; k<strlen(wordie)-2;k++){
-      if (get_side(wordie[k],left,right, top, bottom)==get_side(wordie[k-1],left,right, top, bottom) || get_side(wordie[k],left,right, top, bottom)<0) return false;
+  return true;
+
+
+
+}
+int get_side(char letter, char * left, char * right, char *top, char * bottom){
+  int side = -1;
+  for (int k=0; k<4; k++){
+    if (letter==right[k]){
+       side = 0;
+       break;
     }
-
-    return true;
-
-
-
-  }
-  char get_side(char letter, char * left, char * right, char *top, char * bottom){
-    int side = -1;
-    for (int k=0; k<4; k++){
-      if (letter==right[k]){
-         side = 0;
+    if (letter==left[k]){
+        side = 1;
+        break;
+    }
+    if (letter==top[k]){
+         side = 2;
          break;
-      }
-      if (letter==left[k]){
-          side = 1;
-          break;
-      }
-      if (letter==top[k]){
-           side = 2;
-           break;
-      }
-      if (letter==bottom[k]){
-            side = 3;
-            break;
-      }
     }
-    return side;
+    if (letter==bottom[k]){
+          side = 3;
+          break;
+    }
   }
+  return side;
+}
 
 
 
-  void base(char *letters, char *word, word_t* wordlist){
+void base(char *letters, char *word, word_t* wordlist){
 
-    word_t *wordie = NULL;
-    word_t *runner3 = wordlist;
-    int count =0;
-    if(wordlist->next==NULL){
+  word_t *wordie = NULL;
+  word_t *runner3 = wordlist;
+  int count = 0;
+  if(wordlist->next==NULL){
 
 
     wordie = (word_t*)malloc(sizeof(word_t));
@@ -295,7 +304,6 @@ return 0;
       }
     }
     for (int i=0; i<strlen(word);i++){
-
       for (int j=0;j<13; j++){
         if (update_letters[j]==word[i]){
           update_letters[j] = '0';
@@ -307,19 +315,12 @@ return 0;
 
     for (int i=0; i<13;i++){
       if (update_letters[i]== '0')letters_left++;
-      }
+    }
     //printf("%s\n", letters);
     if (letters_left==12){
       printf("\nPOSSIBLE COMBINATION : \n");
       word_t* printer = wordlist;
       printf("%s->%s", printer->letters, printer->next->letters);
-      }
-
-    //base case
-    //free(wordie);
-
-
-    //free(wordie);
-    //printf("adding %s did not use all the letters\n", word);
+    }
   }
-  }
+}
